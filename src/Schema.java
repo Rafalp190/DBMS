a2e632e9a2311ca6a2ad1a84a6d33b9c4f9920f9
Rafalp190 <leo13361@uvg.edu.gt>
2017-03-23 19:09:57 -0600

    Se completó la logica de llava :camel:

--- src/Schema.java
+++ src/Schema.java
@@ -1,4 +1,47 @@
+import java.io.Serializable;
+import java.util.ArrayList;
 
-public class Schema {
 
+public class Schema implements Serializable {
+
+	private ArrayList<DataBase> dataBases;
+
+	public Schema() {
+		this.dataBases = new ArrayList<DataBase>();
+	}
+
+	/**
+	 * @return the dataBases
+	 */
+	public ArrayList<DataBase> getSchema() {
+		return dataBases;
+	}
+
+	/**
+	 * @param dataBases the dataBases to set
+	 */
+	public void setSchema(ArrayList<DataBase> dataBases) {
+		this.dataBases = dataBases;
+	}
+	
+	public void addDataBase(DataBase b)
+	{
+		this.dataBases.add(b);
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		String res = "Schema:\n";
+		int cont = 1;
+		for (DataBase i: this.dataBases)
+		{
+			res += "\tDB#" + Integer.toString(cont) + " " + i.toString() + "\n";
+			cont++;
+		}
+		return res;
+	}	
+	
 }
